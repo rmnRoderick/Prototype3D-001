e06ac9fc8c42a0895b0e9d16f158774a5b3949ca
rmnRoderick <rodrigomatiasnieva@gmail.com>
2023-08-26 12:19:00 -0300

    add double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,15 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
 
     //GameObject player=null;
-    private Rigidbody rb = null;
     private int jumps = 0;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 movement;
+
     [SerializeField] float jumpForce =10f;
     [SerializeField] float speed =150f;
+    [SerializeField] Rigidbody rb;
 
     private bool onGround = true;
     
@@ -24,42 +29,44 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        onGround = true;
-        jumps = 0;
+
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            onGround = true;
+            jumps = 0;
+        }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        onGround = false;
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            onGround = false;
+        }
     }
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
-        //si esta en el suelo
-        if( onGround)
+        //salto // doble salto
+        if (Input.GetKeyUp(KeyCode.Space) && jumps < 2 )
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+            jumps++;
+            Vector3 salto = new Vector3(0, jumpForce, 0);
+            rb.AddForce(salto, ForceMode.Impulse);
+            Debug.Log("salto");
+        }
 
-            //salto
-            if (Input.GetKeyUp(KeyCode.Space) && jumps <2)
-            {
-                jumps++;
 
-                Vector3 salto = new Vector3(0, jumpForce, 0);
-
-                rb.AddForce(salto, ForceMode.Impulse);
-
-                Console.WriteLine("salto");
-            }
-
+        //si esta en el suelo
+        if ( onGround )
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            movement = new Vector3(horizontalInput, 0, verticalInput);
             rb.AddForce(speed * Time.deltaTime * movement ,  ForceMode.Force);
-
-
         }
         else
         {
@@ -68,7 +75,7 @@ public class Player : MonoBehaviour
             {
                 GameObject.Find("Main Camera").transform.parent = null;
                 Destroy(gameObject);
-                Console.WriteLine("game over");
+                Debug.Log("game over");
             }
 
         }
