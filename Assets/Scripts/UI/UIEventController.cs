e5413724578527b7b535586df8dfc2fd717eedfa
richardpine <65832762+richardpine@users.noreply.github.com>
2023-10-21 09:59:18 -0300

    EventController & StandardHud

---
+++ Assets/Scripts/UI/UIEventController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class UIEventController
+{
+    public Action<string, string> OnShowPanel;
+    public Action OnHidePanel;
+
+    public event Action<string> OnUpdateLifeHUD;
+    public event Action<string> OnUpdateScoreHUD;
+    public event Action<string> OnUpdateTimeHUD;
+}
